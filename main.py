f620d724e790c7013d21f1dd8e8f0e2cf804403d
kouCreates <koucreates@gmail.com>
2024-07-15 18:51:01 +1200

    add sorting

--- main.py
+++ main.py
@@ -5,27 +5,49 @@ def print_out(contents):
 
 def count_words(contents):
     words = contents.split()
-    print(f"There are {len(words)} words in the file")
+    return len(words)
 
 def count_characters(contents):
-    str = contents.lower()
+    contents = contents.lower()
     count_dict = {}
 
     for char in contents:
+        if not char.isalpha(): continue
+
         if char in count_dict:
             count_dict[char] += 1
         else:
             count_dict[char] = 1
 
-    print(count_dict)
+    return count_dict
+
+def sort_on(dict):
+    return dict["count"]
+
+def dict_to_list_of_dicts(char_dict):
+    result = []
+    for char, count in char_dict.items():
+        result.append({"char": char, "count": count})
+    return result
+
+def count_report(list):
+    for item in list:
+        print(f"The '{item['char']}' character was found {item['count']} times")
+
 
 def main():
     with open(path_to_file) as f:
         file_contents = f.read()
     
-    #print_out(file_contents)
-    #count_words(file_contents)
-    count_characters(file_contents)
+    words = count_words(file_contents)
+    count_dict = count_characters(file_contents)
+    count_list = dict_to_list_of_dicts(count_dict)
+    count_list.sort(reverse=True, key=sort_on)
+
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{words} words found in the document\n")
+    count_report(count_list)
+    print("--- End report ---")
 
 if __name__ == '__main__':
     main()
