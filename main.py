f223feaab7b2ab26362be043882c8c650a6ee5fa
kouCreates <koucreates@gmail.com>
2024-07-15 18:04:52 +1200

    add count characters

--- main.py
+++ main.py
@@ -7,13 +7,25 @@ def count_words(contents):
     words = contents.split()
     print(f"There are {len(words)} words in the file")
 
+def count_characters(contents):
+    str = contents.lower()
+    count_dict = {}
+
+    for char in contents:
+        if char in count_dict:
+            count_dict[char] += 1
+        else:
+            count_dict[char] = 1
+
+    print(count_dict)
 
 def main():
     with open(path_to_file) as f:
         file_contents = f.read()
     
-    print_out(file_contents)
-    count_words(file_contents)
+    #print_out(file_contents)
+    #count_words(file_contents)
+    count_characters(file_contents)
 
 if __name__ == '__main__':
     main()
